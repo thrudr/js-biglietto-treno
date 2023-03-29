b4b7b6296abd132515040fbee91207f56fc6dcca
thrudr <ariel.oliveiraesilva@gmail.com>
2023-03-29 16:24:59 +0200

    console.log

--- main.js
+++ main.js
@@ -6,19 +6,29 @@ let kilometres = prompt("How many kilometers to your destination?");
 passagerAge = parseInt(passagerAge);
 kilometres = parseInt(kilometres);
 
+console.log("Your age is: " + passagerAge);
+console.log("The distance is: " + kilometres);
+
 //Ticket information.
 let ticketPrice = 0.21 * kilometres;
 const decimalValue = ticketPrice.toFixed(2);
 let underage = passagerAge < 18;
 let overSixtfifth = passagerAge > 65;
 
+console.log("The ticket price: " + ticketPrice);
+console.log("Ticket price in decimal value: " + decimalValue);
+
 //Ticket price value.
 if(underage) {
 
     let underageDiscount = ticketPrice * 20 / 100;
     let underTotalPrice = decimalValue - underageDiscount;
-} else if {
+} else if (overSixtfifth) {
 
     let overSixtfifthDiscount = ticketPrice * 40 / 100;
     let overTotalPrice = decimalValue - overSixtfifthDiscount;
+} else {
+    
 }
+
+console.log()
