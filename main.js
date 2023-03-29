518eeed49594348a5d3f23c93f2ab6e8e543135e
thrudr <ariel.oliveiraesilva@gmail.com>
2023-03-29 15:45:24 +0200

    passager-information

---
+++ main.js
@@ -0,0 +1,8 @@
+
+//Passager information.
+const passagerAge = prompt("How old are you?");
+let kilometres = prompt("How many kilometers to your destination?");
+
+passagerAge = parseInt(passagerAge);
+kilometres = parseInt(kilometres);
+
